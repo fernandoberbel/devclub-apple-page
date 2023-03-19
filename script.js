b1b7a44bdee393dbe3be05c4a8bcd1048d169350
fberbel <fberbel@gmail.com>
2023-03-19 15:41:32 -0300

    Acrescentado JavaScript e finalizado

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Oi")
+
+function changeColor (cor){
+    let circle = document.querySelector(".circle")
+    circle.style.background = cor
+}
+
+function changeImg (img){
+    let imgIphone = document.querySelector(".img-iphone")
+    imgIphone.src = img
+}
